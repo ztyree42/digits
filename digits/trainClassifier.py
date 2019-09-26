6806360faa3bfe60fe4d7d76ac5cfee58fdb5b8f
Ubuntu <ubuntu@ip-172-31-33-229.ec2.internal>
2019-09-26 22:49:32 +0000

    Separated definition from training code

--- digits/trainClassifier.py
+++ digits/trainClassifier.py
@@ -4,8 +4,12 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 import torchvision as tv
+from digits.transforms.slider import Slider
 from digits.dataLoader import spokenDigitDataset
 import yaml
+from torch.utils.tensorboard import SummaryWriter
+
+writer = SummaryWriter()
 
 with open('/home/ubuntu/projects/digits/digits/classifierParams.yaml') as f:
     args = yaml.load(f, Loader=yaml.FullLoader)
@@ -30,14 +34,6 @@ EPOCHS = t_args['epochs']
 DROP_OUT = t_args['drop_out']
 MODEL_PATH = t_args['model_path']
 
-class Slider():
-    def __init__(self, size, step):
-        self.size = size
-        self.step = step
-    def __call__(self, inp):
-        inp= inp.squeeze().unfold(0, self.size, self.step)
-        return inp
-
 tsfm = tv.transforms.Compose([
     tv.transforms.ToTensor(),
     Slider(8, 4)
@@ -45,20 +41,20 @@ tsfm = tv.transforms.Compose([
 # tsfm = None
 
 trainSet = spokenDigitDataset('/home/ubuntu/projects/spokenDigits',
-                                         DATA_TYPE,
-                                         transform=tsfm,
-                                         train = True)
-
-testSet = spokenDigitDataset('/home/ubuntu/projects/spokenDigits',
                               DATA_TYPE,
                               transform=tsfm,
-                              train=False)
+                              train=True)
+
+testSet = spokenDigitDataset('/home/ubuntu/projects/spokenDigits',
+                             DATA_TYPE,
+                             transform=tsfm,
+                             train=False)
 
 trainLoader = DataLoader(trainSet, batch_size=BATCH_SIZE, shuffle=True,
-                        num_workers=NUM_WORKERS)
+                         num_workers=NUM_WORKERS)
 
 testLoader = DataLoader(testSet, batch_size=BATCH_SIZE, shuffle=False,
-                         num_workers=NUM_WORKERS)
+                        num_workers=NUM_WORKERS)
 
 batch = next(iter(trainLoader))
 
@@ -73,7 +69,8 @@ class LSTM_TAGGER(nn.Module):
         super(LSTM_TAGGER, self).__init__()
         self.hidden_dim = hidden_dim
 
-        self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True)
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers=2,
+                            batch_first=True)
 
         self.hidden2tag = nn.Linear(hidden_dim, target_size)
 
@@ -85,28 +82,41 @@ class LSTM_TAGGER(nn.Module):
 
 
 model = LSTM_TAGGER(INPUT_DIM, HIDDEN_DIM, OUTPUT_DIM)
+model.cuda()
 criterion = nn.NLLLoss()
 optimizer = optim.Adam(model.parameters(), lr=.001, weight_decay=1e-5)
 
-for epoch in range(20):
-    running_loss = 0
+best_loss = None
+for epoch in range(100):
+    train_loss = 0
+    val_loss = 0
     for idx, batch in enumerate(trainLoader):
         model.zero_grad()
+        model.train()
         batch['feature'].requires_grad_()
-        
-        tag_scores = model(batch['feature'].view(BATCH_SIZE, 15, -1))
-        loss = criterion(tag_scores[:,-1,:], batch['label'].detach())
+
+        tag_scores = model(batch['feature'].cuda().view(BATCH_SIZE, 15, -1))
+        loss = criterion(tag_scores[:, -1, :], batch['label'].cuda().detach())
         loss.backward()
         optimizer.step()
-        running_loss += loss / len(trainSet)
+        train_loss += BATCH_SIZE*(loss / len(trainSet))
     with torch.no_grad():
-        inputs = batch['feature']
-        model.eval()
-        tag_scores = model(inputs.view(BATCH_SIZE, 15, -1))
-        # l = tag_scores[-1,-1,:].argmax()
-        acc = (tag_scores[:, -1, :].argmax(1) == batch['label']).sum().item()/BATCH_SIZE
+        for idx, batch in enumerate(testLoader):
+            batch = next(iter(testLoader))
+            inputs = batch['feature'].cuda()
+            tag_scores = model.eval()(inputs.view(BATCH_SIZE, 15, -1))
+            loss = criterion(tag_scores[:, -1, :],
+                             batch['label'].cuda().detach())
+            val_loss += BATCH_SIZE*(loss/len(testSet))
+        writer.add_scalar('classifier/loss/val', val_loss, epoch)
+        acc = (tag_scores[:, -1, :].argmax(1) ==
+               batch['label'].cuda()).sum().item()/BATCH_SIZE
         acc = round(acc, 2)
-        print('Loss: ', running_loss.item())
-        # print('Target: ', batch['label'][-1])
-        # print('Estimate: ', l)
+        print('Loss: ', val_loss.item())
         print('Accuracy: %', acc)
+        if best_loss is None:
+            best_loss = val_loss
+        if val_loss < best_loss:
+            torch.save(model.state_dict(), MODEL_PATH)
+            best_loss = val_loss
+    writer.add_scalar('classifier/loss/train', train_loss, epoch)
